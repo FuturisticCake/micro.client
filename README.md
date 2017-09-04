c83e109c262d2efb5ffd929ea658c353355e2d63
Bennett Somerville <bdsomer@gmail.com>
2017-09-03 18:42:06 -0700

    Clean up code

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # micro.client
 A Minecraft client, completely console (and Node.js) powered.
 
-Note that this project is not actively maintained. It's super old and isn't up-to-par in any way with my current coding skills!
+To start the client, use the following command:
+
+```
+node lib/cli.js <hostname> <port> <username/email>
+```
+
+Commands:
+
+- help - Display the help menu
+- list - list all players on the server
+- chat \<message> - chats the specified message
+- ka - enable killaura
+	- -p - Target players
+	- -m - Target mobs
+	- -t - Enable teleportation
+	- -f - Enable player following
+- dka - Disable killaura
+- cs \<control state> [on/off] (defaults to on) - Alters the control states of the bot
+	- Control States:
+		- forward - Makes the bot move forward
+		- back - Makes the bot move backward
+		- left - Makes the bot move to the left
+		- right - Makes the bot move to the right
+		- jump - Makes the bot jump
+		- sprint - Makes the bot sprint
+
+Requiring the module in code returns a mineflayer killaura plugin.
--- ps-client/auto.js
+++ ps-client/auto.js
@@ -1,43 +1,43 @@
-const ps = require("./ps.js");
-const mc = require("mineflayer");
+const ps = require('./ps.js')
+const mc = require('mineflayer')
 
-const host = "s1.hosthorde.com";
+const host = 's1.hosthorde.com'
 
-var scanPort = 25970;
+var scanPort = 25970
 
-console.log("Starting MC port scanner...");
+console.log('Starting MC port scanner...')
 
 var scanNext = () => {
-	scanPort++;
+	scanPort++
 	ps(host, scanPort, (portOpen) => {
 		if (!portOpen) {
-			console.log("x " + scanPort + " - Port not open.");
-			scanNext();
+			console.log('x ' + scanPort + ' - Port not open.')
+			scanNext()
 		}
 		else {
 			try {
 				var mcClient = mc.createBot({
-					"username": "offlineBotAccount",
-					"host": host,
-					"port": scanPort
-				});
+					'username': 'offlineBotAccount',
+					'host': host,
+					'port': scanPort
+				})
 
-				mcClient.on("spawn", () => {
-					console.log("Y - " + scanPort + " - Client spawned into game. Version: " + mcClient.version + ", Player count: " + mcClient.players.length);
-					mcClient.end();
-					scanNext();
-				});
+				mcClient.on('spawn', () => {
+					console.log('Y - ' + scanPort + ' - Client spawned into game. Version: ' + mcClient.version + ', Player count: ' + mcClient.players.length)
+					mcClient.end()
+					scanNext()
+				})
 
-				mcClient.on("kicked", (kickMessage) => {
-					console.log("x - " + scanPort + " - Client kicked: " + kickMessage + ". Version: " + mcClient.version);
-					scanNext();
-				});
+				mcClient.on('kicked', (kickMessage) => {
+					console.log('x - ' + scanPort + ' - Client kicked: ' + kickMessage + '. Version: ' + mcClient.version)
+					scanNext()
+				})
 			}
 			catch (err) {
-				console.log("Error connecting bot to port " + scanPort);
+				console.log('Error connecting bot to port ' + scanPort)
 			}
 		}
-	});
-};
+	})
+}
 
-scanNext();
+scanNext()
